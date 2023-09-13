1b9bb825e43e1029f1aa364c1dbc8f0279e3c899
JensErven <jens.erven@student.ehb.be>
2023-09-13 12:50:45 +0200

    BUG window undefined is solved

--- src/components/Shared/BlobContainer.tsx
+++ src/components/Shared/BlobContainer.tsx
@@ -10,42 +10,49 @@ import {
 const BlobContainer = () => {
   const controls = useAnimation();
   const mouseX = useMotionValue(0); // Initialize a motion value for the mouse x position
-  const containerX = useTransform(mouseX, [0, window.innerWidth], [-50, 50]);
 
   useEffect(() => {
-    const numBlobs = 15; // Adjust the number of blobs
-    const blobContainer = document.getElementById("blob-container");
+    if (typeof window !== "undefined") {
+      const numBlobs = 15; // Adjust the number of blobs
+      const blobContainer = document.getElementById("blob-container");
 
-    for (let i = 0; i < numBlobs; i++) {
-      const blob = document.createElement("div");
-      blob.className = "random-blob";
-      blob.style.left = `${Math.random() * 100}vw`; // Random horizontal position
-      blob.style.top = `${Math.random() * 100}vh`; // Random vertical position
+      for (let i = 0; i < numBlobs; i++) {
+        const blob = document.createElement("div");
+        blob.className = "random-blob";
+        blob.style.left = `${Math.random() * 100}vw`; // Random horizontal position
+        blob.style.top = `${Math.random() * 100}vh`; // Random vertical position
 
-      // Randomize blob sizes between 50px and 150px
-      const blobSize = Math.random() * (150 - 50) + 150;
-      blob.style.width = `${blobSize}px`;
-      blob.style.height = `${blobSize}px`;
+        // Randomize blob sizes between 50px and 150px
+        const blobSize = Math.random() * (150 - 50) + 100;
+        blob.style.width = `${blobSize}px`;
+        blob.style.height = `${blobSize}px`;
 
-      blobContainer.appendChild(blob);
-      console.log("blobs come here!");
-      // Add animation delay and duration
-      blob.style.animationDuration = `${Math.random() * 10 + 5}s`;
-      blob.style.animationDelay = `${Math.random() * 5}s`;
+        blobContainer.appendChild(blob);
+
+        // Add animation delay and duration
+        blob.style.animationDuration = `${Math.random() * 10 + 5}s`;
+        blob.style.animationDelay = `${Math.random() * 5}s`;
+      }
+
+      // Update mouseX motion value with mouse x position
+      const handleMouseMove = (e) => {
+        mouseX.set(e.clientX);
+      };
+
+      window.addEventListener("mousemove", handleMouseMove);
+
+      // Clean up event listener
+      return () => {
+        window.removeEventListener("mousemove", handleMouseMove);
+      };
     }
-
-    // Update mouseX motion value with mouse x position
-    const handleMouseMove = (e) => {
-      mouseX.set(e.clientX); // Update mouseX value with mouse x position
-    };
-
-    window.addEventListener("mousemove", handleMouseMove);
-
-    // Clean up event listener
-    return () => {
-      window.removeEventListener("mousemove", handleMouseMove);
-    };
   }, [mouseX]);
+
+  const containerX = useTransform(
+    mouseX,
+    [0, typeof window !== "undefined" ? window.innerWidth : 0],
+    [-50, 50]
+  );
 
   return (
     <motion.div
